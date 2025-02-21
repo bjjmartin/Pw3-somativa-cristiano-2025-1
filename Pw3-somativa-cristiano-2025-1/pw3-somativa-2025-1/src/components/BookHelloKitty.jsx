d6e5591f7d0f2cad189fc6b95df0450ad04c036f
xi 3 <168142156+bjjmartin@users.noreply.github.com>
2025-02-20 22:37:29 -0300

    NavBar

--- Pw3-somativa-cristiano-2025-1/pw3-somativa-2025-1/src/components/BookHelloKitty.jsx
+++ Pw3-somativa-cristiano-2025-1/pw3-somativa-2025-1/src/components/BookHelloKitty.jsx
@@ -1,9 +1,14 @@
+import Button from './Button'
+
+import style from "./BookCard.module.css"
+
 const BookHelloKitty = ({titulo, autor, imagem}) => {
     return(
-    <div>
-        <h3> {titulo} </h3>
-        <p> {autor} </p>
+    <div className= {style.bookCard}>
+        <h3 className= {style.titulo}> {titulo} </h3>
+        <p className= {style.autor}> {autor} </p>
         <img src={imagem} alt= 'Capa: As aventuras da Hello kitty' />
+        <Button label='DETALHE'/>
     </div>
     )
 }
---
+++ Pw3-somativa-cristiano-2025-1/pw3-somativa-2025-1/src/components/layout/NavBar.jsx
@@ -0,0 +1,15 @@
+import style from './NavBar.module.css'
+
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CADASTRAR LIVRO</li>
+                <li>LISTA LIVROS</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
